4a57c5b9727fc620e97ee7d8cb8691e676d1e231
Roshan <nkroshankumar@gmail.com>
2022-11-28 20:58:24 +0530

    type for CircleColorComp created

--- client/typings/typings.d.ts
+++ client/typings/typings.d.ts
@@ -4,7 +4,24 @@ interface OnDraw {
     ctx: CanvasRenderingContext2D
 }
 
+interface OnDrawSocketProps {
+    currentPoints: Points
+    prePoints: Points | null
+    color: string
+    size: SizeProps[size]
+}
+
 interface Points {
     x: number
     y: number
+}
+
+interface ColorCicleProps {
+    style: "small" | "mid" | "large"
+    size: SizeProps[size]
+    setSize: (size: SizeProps[size]) => void
+}
+
+interface SizeProps {
+    size: 5 | 7.5 | 10
 }
