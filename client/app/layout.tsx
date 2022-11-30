b4f2d983120e9d5eb82d4ccbd7bdc071f18bb532
Roshan <nkroshankumar@gmail.com>
2022-11-30 21:11:36 +0530

    join and create room page created and context added

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,18 +1,36 @@
-import './globals.css'
+"use client"
+
+import { createContext, useState } from "react"
+import "./globals.css"
+import { Water_Brush } from "@next/font/google"
+
+const font = Water_Brush({ weight: ["400"], variable: "--font-sketch" })
+
+export const UserContext = createContext({} as UserContext)
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      {/*
+    const [user, setUser] = useState({} as UserProps)
+    return (
+        <html lang="en" className={"font-disp"}>
+            {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+            <head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Cabin+Sketch:wght@400;700&display=swap"
+                    rel="stylesheet"
+                ></link>
+            </head>
+            <body>
+                <UserContext.Provider value={{ user, setUser }}>
+                    {children}
+                </UserContext.Provider>
+            </body>
+        </html>
+    )
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,148 +1,79 @@
 "use client"
 
-import { useEffect, useState } from "react"
-import useDraw from "../hooks/useDraw"
-import { CirclePicker } from "react-color"
-import { io } from "socket.io-client"
-import onDraw from "../utils/onDraw"
-import getCanvasSize from "../utils/getCanvasSize"
-
-const socket = io("https://next-paint-io.onrender.com")
+import { useContext, useEffect, useState } from "react"
+import { customAlphabet } from "nanoid"
+import { UserContext } from "./layout"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
-    const [color, setColor] = useState("#000")
-    const [size, setSize] = useState<5 | 7.5 | 10>(5)
+    const nanoid = customAlphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", 4)
 
-    const onCreate = ({ currentPoints, ctx, prePoints }: OnDraw) => {
-        socket.emit("onDraw", { currentPoints, prePoints, color, size })
+    const { user, setUser } = useContext(UserContext)
+    const [name, setName] = useState("")
+    const [joinId, setJoinId] = useState("")
+    const [nameError, setNameError] = useState(false)
+    const [joinIdError, setJoinIdError] = useState(false)
+    const router = useRouter()
 
-        onDraw({ currentPoints, ctx, prePoints, color, size })
+    const handleCreateRoom = () => {
+        if (!name) return setNameError(true)
+        setNameError(false)
+        const roomId = nanoid()
+        setUser({ name, roomId, members: [], leader: name })
+        router.push(`/room/${roomId}`)
     }
 
-    const { canvasRef, onMouseDown, handleClear } = useDraw(onCreate)
-    const [canvasSize, setCanvasSize] = useState({ width: 500, height: 500 })
-
-    useEffect(() => {
-        const { height, width } = getCanvasSize()
-        setCanvasSize({ height, width })
-    }, [])
-
-    useEffect(() => {
-        const ctx = canvasRef.current?.getContext("2d")
-
-        socket.emit("client-ready")
-
-        socket.on("get-state", () => {
-            if (!canvasRef.current?.toDataURL()) return
-
-            socket.emit("canvas-state", canvasRef.current.toDataURL())
-        })
-
-        socket.on("canvas-state-from-server", (state: string) => {
-            const img = new Image()
-            img.src = state
-            img.onload = () => {
-                ctx?.drawImage(img, 0, 0)
-            }
-        })
-
-        socket.on("onDraw", ({ currentPoints, prePoints, color, size }) => {
-            if (!ctx) return
-
-            onDraw({ currentPoints, ctx, prePoints, color, size })
-        })
-
-        socket.on("handleClear", handleClear)
-
-        return () => {
-            socket.off("get-state")
-            socket.off("canvas-state-from-server")
-            socket.off("onDraw")
-            socket.off("handleClear")
-        }
-    }, [canvasRef])
+    const handleJoinRoom = () => {
+        if (!name) return setNameError(true)
+        setNameError(false)
+        if (!joinId || joinId.length < 4) return setJoinIdError(true)
+        setJoinIdError(false)
+        setUser({ name, roomId: joinId, members: [], leader: "" })
+        router.push(`/room/${joinId}`)
+    }
 
     return (
         <main
             style={{ backgroundImage: "url(bg.svg)", backgroundSize: "cover" }}
-            className="min-h-screen py-10 lg:min-h-[90vh] lg:h-screen "
+            className="min-h-screen py-10 lg:min-h-[90vh] lg:h-screen w-full"
         >
             {/*   Title    */}
             <h1 className="p-2 mx-auto text-3xl font-bold text-center text-white bg-black rounded-md md:text-5xl w-max">
                 Next-Paint.io
             </h1>
-            <div className="flex flex-col items-center justify-center mt-20 lg:flex-row gap-x-10 ">
-                <div className="flex flex-col gap-4">
-                    <div className="flex p-4 pt-2 bg-white border-2 border-black rounded-lg gap-x-6">
-                        {/*        Color Picker  */}
-                        <div>
-                            <h1 className="pb-3 text-xl text-center">
-                                Pick Color
-                            </h1>
-                            <CirclePicker
-                                color={color}
-                                onChange={(e) => setColor(e.hex)}
-                            />
-                        </div>
-                        {/*         Size Picker     */}
-                        <div>
-                            <h1 className="pb-3 text-xl text-center">Size</h1>
-                            <div
-                                style={{ color: color }}
-                                className="flex flex-col items-center justify-center gap-y-5"
-                            >
-                                <ColorCircle
-                                    style={"small"}
-                                    size={size}
-                                    setSize={setSize}
-                                />
-                                <ColorCircle
-                                    style={"mid"}
-                                    size={size}
-                                    setSize={setSize}
-                                />
-                                <ColorCircle
-                                    style={"large"}
-                                    size={size}
-                                    setSize={setSize}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <button
-                        onClick={() => socket.emit("handleClear")}
-                        className="px-8 py-2 m-2 bg-white border-2 border-black rounded-md"
-                    >
-                        Clear
-                    </button>
-                </div>
-                <canvas
-                    ref={canvasRef}
-                    onMouseDown={onMouseDown}
-                    className="bg-white border-4 border-black rounded-md"
-                    width={canvasSize.width}
-                    height={canvasSize.height}
+            <div className="flex flex-col p-6 mx-auto mt-20 rounded-lg borer-2 glass md:p-10 gap-y-6 w-max ">
+                <input
+                    type="text"
+                    placeholder="Name *"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="p-2 text-lg border-2 border-black rounded-md w-60 md:w-80 outline-0 btn-shadow"
                 />
+                {nameError && <p className="text-red-600">Enter Name</p>}
+                <input
+                    type="text"
+                    placeholder="Room ID"
+                    value={joinId}
+                    onChange={(e) => setJoinId(e.target.value)}
+                    className="p-2 text-lg border-2 border-black rounded-md w-60 md:w-80 outline-0 btn-shadow"
+                />
+                {joinIdError && (
+                    <p className="text-red-600">Enter Valid Room ID </p>
+                )}
+
+                <button
+                    onClick={handleJoinRoom}
+                    className="block w-full p-2 mx-auto text-2xl font-semibold text-center text-white transition-all bg-blue-500 rounded-md md:text-3xl btn-shadow hover:scale-105 active:scale-90"
+                >
+                    Join Room
+                </button>
+                <button
+                    onClick={handleCreateRoom}
+                    className="block w-full p-2 mx-auto text-2xl font-semibold text-center text-white transition-all rounded-md bg-emerald-500 md:text-3xl btn-shadow hover:scale-105 active:scale-90"
+                >
+                    Create Room
+                </button>
             </div>
         </main>
-    )
-}
-
-const ColorCircle = ({ style, setSize, size }: ColorCicleProps) => {
-    const circleSize = style == "small" ? 1 : style == "mid" ? 1.5 : 2.25
-    const brushSize = style == "small" ? 5 : style == "mid" ? 7.5 : 10
-
-    const handleSize = () => {
-        setSize(brushSize)
-    }
-
-    return (
-        <div
-            onClick={handleSize}
-            style={{ width: `${circleSize}rem`, height: `${circleSize}rem` }}
-            className={` text-inherit rounded-full hover:scale-125 transition-all cursor-pointer
-            ${size == brushSize ? "border-[3px] border-current" : "bg-current"}
-            `}
-        ></div>
     )
 }
